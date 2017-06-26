ebeaa5cdeccec00fbfdd104482ee5a08ba473775
Gustavo Giserman <gugiserman@gmail.com>
2017-06-26 20:56:19 -0300

    Remove age from Intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { getMyAge } from 'utils/'
 import { SectionIcon } from 'commons/'
 import userSVG from 'assets/icons/user.svg'
-
-
-const age = getMyAge()
 
 const Intro = () => (
   <section className="pa3 pa4-ns lh-copy">
     <h2 className="db ma0 mb3 f5 f4-ns mb1-ns">
       <SectionIcon icon={userSVG} description="User icon" />
       <span className="v-base">
-        I'm a minimalist {age}-year-old brazilian developer and passionate about web apps.
+        I'm a minimalist brazilian developer and passionate about web apps.
       </span>
     </h2>
     <p className="db mb3 mb1-ns">
